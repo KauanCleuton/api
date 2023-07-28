e620d0265c714a2f09ceb68920e9a809d1ac3a2a
KauanCleuton <kauancleuton733@gmail.com>
2023-07-28 16:53:02 -0300

    Atualizando

---
+++ src/database/migrations/1690573881140-CreateCategories.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateCategories1690573881140 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
